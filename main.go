4476cc2d9ae9e24ebbc228ee8c4322fd6b58c2f4
Mario Campos <mario-campos@github.com>
2024-03-07 14:50:46 -0600

    Replace 'checksum' command with 'md5sum' command
    I think it makes more sense to have this output be piped to `md5sum` than to execute it, given that there are some platform differences with `md5sum`.

--- main.go
+++ main.go
@@ -62,8 +62,8 @@ func main() {
 	subcommandDownload := flaggy.NewSubcommand("download")
 	flaggy.AttachSubcommand(subcommandDownload, 1)
 
-	subcommandChecksum := flaggy.NewSubcommand("checksum")
-	flaggy.AttachSubcommand(subcommandChecksum, 1)
+	subcommandMD5sum := flaggy.NewSubcommand("md5sum")
+	flaggy.AttachSubcommand(subcommandMD5sum, 1)
 
 	flaggy.SetName("torbox")
 	flaggy.DefaultParser.DisableShowVersionWithVersion()
@@ -174,34 +174,16 @@ func main() {
 		return
 	}
 
-	if subcommandChecksum.Used {
+	if subcommandMD5sum.Used {
 		if err := json.NewDecoder(os.Stdin).Decode(&ttl); err != nil {
 			Error("failed to decode standard input as JSON: %s", err)
 		}
-
-		// Unfortunately, the md5sum command does not support the standard convention
-		// of a single hyphen character '-' to indicate that it should read from standard input.
-		// Instead, we must use the /dev/stdin device file, which is not supported everywhere.
-		cmd := exec.Command("md5sum", "-c", "/dev/stdin")
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		hashpipe, err := cmd.StdinPipe()
-		if err != nil {
-			Error("failed to create pipe to md5sum: %s", err)
-		}
-
 		for _, torrent := range ttl.Data {
 			for _, torrentfile := range torrent.Files {
-				hashpipe.Write([]byte(fmt.Sprintf("%s  %s\n", torrentfile.MD5, torrentfile.Name)))
+				// This format is compatible with md5sum(1):
+				//    <md5sum>  <filename>
+				fmt.Printf("%s  %s\n", torrentfile.MD5, torrentfile.Name)
 			}
-		}
-		if err = hashpipe.Close(); err != nil {
-			Warn("failed to close pipe to md5sum: %s", err)
-		}
-
-		Info("executing command: %s", cmd.String())
-		if err := cmd.Run(); err != nil {
-			Error("failed to execute md5sum: %s", err)
 		}
 		return
 	}
